155f73ccf8c888d363da95f22e0b889a00fb2381
Matthew Martin <zhintateo19924559@gmail.com>
2021-07-24 06:24:10 -0500

    Started with the Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React, { useState } from 'react';
 import logo from './assets/img/evolution_vaporz_nashville001008.jpg';
-import {Jumbotron, Container, TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col  } from 'reactstrap';
+import {
+  Jumbotron,
+  Container,
+  TabContent,
+  TabPane,
+  Nav, 
+  NavItem, 
+  NavLink, 
+  Card, 
+  Button, 
+  CardTitle, 
+  CardText, 
+  Row, 
+  Col  
+} from 'reactstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+
+import Home from './components/Homepage/home';
 
 function App() {
  const [activeTab, setActiveTab] = useState('1');
@@ -11,49 +33,63 @@ function App() {
   }
 
   return (
-    <Container className="">
-      <Jumbotron className="col-md-12">
-        <img src={logo} style={{width: '100%'}}/>
-      </Jumbotron>
-      <Nav tabs style={{justifyContent: 'center'}}>
-          <NavItem >
-            <NavLink
-            /*className={classnames({ active: activeTab === '1' })} */
-            onClick={() => { toggle('1');}}
-            >
-            Home
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink 
-            onClick={() => { toggle('2');}}
-            >
-            E-Juice
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink 
-            onClick={() => { toggle('3');}}
-            >
-            Coupon
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink 
-            onClick={() => { toggle('4');}}
-            >
-            About Us
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink 
-            onClick={() => { toggle('5');}}
-            >
-            Contact Us
-            </NavLink>
-          </NavItem>
-        </Nav>
-    </Container>
+    <Router>
+      <Container className="">
+        <Jumbotron className="col-md-12">
+          <img src={logo}
+            style={{width: '100%'}}
+         />
+        </Jumbotron>
+        <Nav tabs style={{justifyContent: 'center'}}>
+            <NavItem >
+              <NavLink
+              /*className={classnames({ active: activeTab === '1' })} */
+              onClick={() => { toggle('1');}}
+              >
+              Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink 
+              onClick={() => { toggle('2');}}
+              >
+              E-Juice
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink 
+              onClick={() => { toggle('3');}}
+              >
+              Coupon
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink 
+              onClick={() => { toggle('4');}}
+              >
+              About Us
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink 
+              onClick={() => { toggle('5');}}
+              >
+              Contact Us
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <Col md="4">
+
+          </Col>
+          <Col md="8">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Col>
+      </Container>
+    </Router>
   );
 }
 
--- src/components/Homepage/home.tsx
+++ src/components/Homepage/home.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import {
+	Card,
+	CardTitle,
+	CardBody
+} from 'reactstrap';
 
 const Home = () => {
-	return null
+	return (
+		<Card>
+			<CardTitle style={{ width: '100%'}}>
+				<h3>
+					E-volution VaporZ is located in Nashville, Tennessee at 2221 Murfreesboro Pike 37217. We carry a wide variety of Vaping supplies and Mods, for your E-Cigarette. Come by and see us, or give us a call at 615-818-0530. You won't be disappointed. Must be 21 or older to purchase.
+				</h3>
+			</CardTitle>
+		</Card>
+		)
 }
 
 export default Home;
