fb7ce00d31c6fc9b7f6ed3ac2a801fb24f211a77
Matthew Martin <zhintateo19924559@gmail.com>
2021-08-18 07:32:45 -0500

    centered the homepage text

--- src/components/Homepage/home.tsx
+++ src/components/Homepage/home.tsx
@@ -7,7 +7,7 @@ import {
 
 const Home = () => {
 	return (
-		<Card style={{width: '80%' }}>
+		<Card style={{width: '80%', margin: '10rem auto' }}>
 			<CardTitle style={{ width: '100%',justifyContent: 'center'}}>
 				<h3>
 					E-volution VaporZ is located in Nashville, Tennessee at 2221 Murfreesboro Pike 37217. We carry a wide variety of Vaping supplies and Mods, for your E-Cigarette. Come by and see us, or give us a call at 615-818-0530. You won't be disappointed. Must be 21 or older to purchase.
